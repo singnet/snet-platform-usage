e95d9af3b20bf65626c981973bbc0232f2cc6492
Pratik Prajapati <pratikap98@gmail.com>
2019-08-12 14:20:17 +0530

    freecalls nam4e change for api

--- metering/services.py
+++ metering/services.py
@@ -16,7 +16,7 @@ class UsageService(object):
             total_calls = 0
 
         return {"username": username, "org_id": org_id, "service_id": service_id, "total_calls_made": total_calls,
-                "free_calls": free_calls}
+                "free_calls_allowed": free_calls}
 
     def save_usage_details(self, usage_details_dict):
         # nedd to introduce entities when we enhance  feature to this service right now directly using dicts
