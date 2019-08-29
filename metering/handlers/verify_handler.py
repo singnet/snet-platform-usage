6801c2eebad1901136df7bd931e8085022c18a96
Restyled.io <commits@restyled.io>
2019-08-29 11:58:19 +0000

    Restyled by autopep8

--- metering/handlers/verify_handler.py
+++ metering/handlers/verify_handler.py
@@ -5,7 +5,7 @@ from utils import make_response
 
 
 def main(event, context):
-    return  make_response(
-            status_code=StatusCode.SUCCESS_GET_CODE,
-            body=json.dumps({"data":"success"})
-        )
+    return make_response(
+        status_code=StatusCode.SUCCESS_GET_CODE,
+        body=json.dumps({"data": "success"})
+    )
